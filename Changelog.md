1d14ac877c74cb7abfa992052d2c32cf8152f985
Yusuf Olokoba <olokobayusuf@gmail.com>
2024-01-05 11:11:40 -0500

    Happy new year!

--- Changelog.md
+++ Changelog.md
@@ -1,2 +1,5 @@
+## 0.0.2
++ Added `FXNProfile.h` header for prediction profiling.
+
 ## 0.0.1
 + First pre-release.
--- FXNProfile.h
+++ FXNProfile.h
@@ -3,7 +3,7 @@
 //  Function
 //
 //  Created by Yusuf Olokoba on 11/23/2023.
-//  Copyright © 2023 NatML Inc. All rights reserved.
+//  Copyright © 2024 NatML Inc. All rights reserved.
 //
 
 #pragma once
